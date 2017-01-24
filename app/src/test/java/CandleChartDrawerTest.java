aa23d155fde883d1367471a42f87d245695edb1e
Hortensie <szczesny.piotr@outlook.com>
2017-01-24 23:36:26 +0100

    added 2 unit tests for CandleChartDrawer.class (prepareChart & adjustCandleSet methods)

--- app/src/test/java/CandleChartDrawerTest.java
+++ app/src/test/java/CandleChartDrawerTest.java
@@ -1,6 +1,9 @@
+import android.graphics.Paint;
+
 import com.github.mikephil.charting.charts.CandleStickChart;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.CandleDataSet;
 import com.vaadin.polymer.demo.client.sampler.ai_trader.CandleChartDrawer;
 
 import org.junit.Assert;
@@ -8,6 +11,9 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static android.graphics.Color.BLACK;
+import static android.graphics.Color.GREEN;
+import static android.graphics.Color.RED;
+import static android.graphics.Color.WHITE;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -56,5 +62,26 @@ public class CandleChartDrawerTest {
         verify(yAxis).setTextColor(BLACK);
     }
 
+    @Test
+    public void validatePrepareChart(){
+
+        sut.prepareChart(candleStickChart);
+        verify(candleStickChart).setBackgroundColor(WHITE);
+        verify(candleStickChart).setMaxVisibleValueCount(60);
+        verify(candleStickChart).setPinchZoom(false);
+        verify(candleStickChart).setAutoScaleMinMaxEnabled(true);
+    }
+
+    @Test
+    public void validateAdjustCandleSet(){
+        CandleDataSet set = mock(CandleDataSet.class);
+        sut.adjustCandleDataSet(set);
+        verify(set).setShadowColor(BLACK);
+        verify(set).setShadowWidth(0.7f);
+        verify(set).setDecreasingColor(RED);
+        verify(set).setDecreasingPaintStyle(Paint.Style.FILL);
+        verify(set).setIncreasingColor(GREEN);
+        verify(set).setIncreasingPaintStyle(Paint.Style.FILL);
+    }
 
 }
