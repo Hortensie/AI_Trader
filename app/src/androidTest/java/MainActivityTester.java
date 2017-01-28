5ea05c65d5986bc0123f157c576d8dc6ef362c52
Hortensie <szczesny.piotr@outlook.com>
2017-01-28 07:09:21 +0100

    added simple MainActivity test to check AI trader button

--- app/src/androidTest/java/MainActivityTester.java
+++ app/src/androidTest/java/MainActivityTester.java
@@ -13,8 +13,6 @@ import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.scrollTo;
-import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withParent;
@@ -23,20 +21,33 @@ import static org.hamcrest.Matchers.allOf;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class MainTraderAndroidTest {
+public class MainActivityTester {
 
     @Rule
     public ActivityTestRule<MainOffline> mActivityTestRule = new ActivityTestRule<>(MainOffline.class);
 
     @Test
-    public void mainTraderAndroidTest() {
-
+    public void mainActivityTester() {
         ViewInteraction button3 = onView(
-                allOf(ViewMatchers.withId(R.id.buttonPeriodSelector), withText("Period selector"),
+                allOf(ViewMatchers.withId(R.id.buttonGetSymbolsApi), withText("Get symbols from db"),
                         withParent(allOf(withId(R.id.activity_main_live),
                                 withParent(withId(android.R.id.content)))),
                         isDisplayed()));
         button3.perform(click());
+
+        ViewInteraction button4 = onView(
+                allOf(withId(R.id.buttonGetSymbols), withText("Get Symbols from API"),
+                        withParent(allOf(withId(R.id.activity_main_live),
+                                withParent(withId(android.R.id.content)))),
+                        isDisplayed()));
+        button4.perform(click());
+
+        ViewInteraction button5 = onView(
+                allOf(withId(R.id.buttonPeriodSelector), withText("Period selector"),
+                        withParent(allOf(withId(R.id.activity_main_live),
+                                withParent(withId(android.R.id.content)))),
+                        isDisplayed()));
+        button5.perform(click());
 
         ViewInteraction radioButton = onView(
                 allOf(withId(R.id.PERIOD_H1), withText("PERIOD_H1"),
@@ -45,59 +56,20 @@ public class MainTraderAndroidTest {
                         isDisplayed()));
         radioButton.perform(click());
 
-        ViewInteraction button4 = onView(
-                allOf(withId(R.id.buttonPickStartDate), withText("Set Start Date"),
-                        withParent(allOf(withId(R.id.activity_x_api_trading_input),
-                                withParent(withId(android.R.id.content)))),
-                        isDisplayed()));
-        button4.perform(click());
-
-        ViewInteraction button5 = onView(
-                allOf(withId(android.R.id.button1), withText("OK")));
-        button5.perform(scrollTo(), click());
-
-        ViewInteraction button6 = onView(
-                allOf(withId(android.R.id.button1), withText("OK")));
-        button6.perform(scrollTo(), click());
-
         ViewInteraction button7 = onView(
-                allOf(withId(R.id.buttonPickEndDate), withText("Set End Date"),
+                allOf(withId(R.id.buttonHistData), withText("Get Historical Data"),
                         withParent(allOf(withId(R.id.activity_x_api_trading_input),
                                 withParent(withId(android.R.id.content)))),
                         isDisplayed()));
         button7.perform(click());
 
         ViewInteraction button8 = onView(
-                allOf(withId(android.R.id.button2), withText("Cancel")));
-        button8.perform(scrollTo(), click());
-
-        ViewInteraction button9 = onView(
-                allOf(withId(R.id.buttonSetSymbol), withText("Set Symbol"),
-                        withParent(allOf(withId(R.id.activity_x_api_trading_input),
-                                withParent(withId(android.R.id.content)))),
-                        isDisplayed()));
-        button9.perform(click());
-
-        ViewInteraction recyclerView = onView(
-                allOf(withId(R.id.my_recycler_view),
-                        withParent(allOf(withId(R.id.activity_trading_symbol),
-                                withParent(withId(android.R.id.content)))),
-                        isDisplayed()));
-        recyclerView.perform(actionOnItemAtPosition(4, click()));
-
-        ViewInteraction button10 = onView(
-                allOf(withId(R.id.buttonHistData), withText("Get Historical Data"),
-                        withParent(allOf(withId(R.id.activity_x_api_trading_input),
-                                withParent(withId(android.R.id.content)))),
-                        isDisplayed()));
-        button10.perform(click());
-
-        ViewInteraction button11 = onView(
                 allOf(withId(R.id.drawCandleChart), withText("Draw Chart"),
                         withParent(allOf(withId(R.id.activity_x_api_trading_input),
                                 withParent(withId(android.R.id.content)))),
                         isDisplayed()));
-        button11.perform(click());
+        button8.perform(click());
+
     }
 
 }
