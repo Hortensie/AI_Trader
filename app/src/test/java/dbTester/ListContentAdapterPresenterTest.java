b5f763a25fde55763c289a52a3d78494a6cc2ee7
Piotr Szczesny <szczesny.piotr@outlook.com>
2017-04-03 22:47:31 +0200

    improved unit test for ListContentAdapterPresenter class
    -MyViewHolder constructor
    -getItemCount()

--- app/src/test/java/dbTester/ListContentAdapterPresenterTest.java
+++ app/src/test/java/dbTester/ListContentAdapterPresenterTest.java
@@ -3,10 +3,15 @@ package dbTester;
 import android.content.Context;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.hortensie.ai_trader.R;
 import com.hortensie.ai_trader.aiViewer.model.CandleEntryRecord;
 import com.hortensie.ai_trader.dbTester.presenter.ListContentAdapterPresenter;
+import com.hortensie.ai_trader.dbTester.view.Fragments.CardContentFragment;
 import com.hortensie.ai_trader.xAPI.ListSymbolRecord;
 
 import org.junit.Assert;
@@ -20,6 +25,7 @@ import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
 
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -37,7 +43,12 @@ public class ListContentAdapterPresenterTest {
     private ListSymbolRecord singleEntry;
     private Resources resources;
     private TypedArray array;
-
+    private ListContentAdapterPresenter.MyViewHolder holder;
+    private ViewGroup parent;
+    private LayoutInflater inflater;
+    private RecyclerView.ViewHolder viewHolder;
+    ListContentAdapterPresenter.MyViewHolder object;
+    View view;
 
     @Before
     public void setUp(){
@@ -45,10 +56,26 @@ public class ListContentAdapterPresenterTest {
         singleEntry=mock(ListSymbolRecord.class);
         resources=mock(Resources.class);
         array=mock(TypedArray.class);
+        holder=mock(ListContentAdapterPresenter.MyViewHolder.class);
+        parent=mock(ViewGroup.class);
+        inflater=mock(LayoutInflater.class);
+        viewHolder=mock(RecyclerView.ViewHolder.class);
+        object=mock(ListContentAdapterPresenter.MyViewHolder.class);
+        view=mock(View.class);
     }
 
     @Test
     public void symbolSetterGetter(){
+
+        ListContentAdapterPresenter.setTemp_symbol("input");
+        Assert.assertEquals("input",ListContentAdapterPresenter.getTemp_symbol());
+
+
+    }
+
+    @Test
+    public void constructorShouldInit(){
+
         //given
         final List<ListSymbolRecord> list = new LinkedList<>();
         list.add(singleEntry);
@@ -73,10 +100,34 @@ public class ListContentAdapterPresenterTest {
         //assert that ListContentAdapterPresenter object is not null
         Assert.assertNotNull(sut);
 
-        ListContentAdapterPresenter.setTemp_symbol("input");
-        Assert.assertEquals("input",ListContentAdapterPresenter.getTemp_symbol());
-
-
     }
 
+    @Test
+    public void MyViewHolderConstructor(){
+
+        //indirect inputs
+        //when
+        when(context.getResources()).thenReturn(resources);
+        when(resources.obtainTypedArray(R.array.place_avator)).thenReturn(array);
+        when(inflater.inflate(R.layout.item_list,parent,false)).thenReturn(view);
+
+        holder = new ListContentAdapterPresenter(context,recordList).new MyViewHolder(inflater,parent);
+
+        //then
+        Assert.assertNotNull(holder);
+    }
+
+    @Test
+    public void validateGetItemCount(){
+
+        //indirect inputs
+        //when
+        when(context.getResources()).thenReturn(resources);
+        when(resources.obtainTypedArray(R.array.place_avator)).thenReturn(array);
+
+        sut=new ListContentAdapterPresenter(context,recordList);
+
+        //then
+        Assert.assertEquals(20,sut.getItemCount());
+    }
 }
